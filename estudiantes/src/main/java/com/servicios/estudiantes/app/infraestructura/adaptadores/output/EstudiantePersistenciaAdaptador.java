aada4be18d1f7eefea39ff47838a5771b89b4685
Jesús Joel Meneses Meneses <73592097+joel92MM@users.noreply.github.com>
2025-06-06 17:41:49 +0200

    Se avanza en el curso

--- estudiantes/src/main/java/com/servicios/estudiantes/app/infraestructura/adaptadores/output/EstudiantePersistenciaAdaptador.java
+++ estudiantes/src/main/java/com/servicios/estudiantes/app/infraestructura/adaptadores/output/EstudiantePersistenciaAdaptador.java
@@ -2,29 +2,39 @@ package com.servicios.estudiantes.app.infraestructura.adaptadores.output;
 
 import com.servicios.estudiantes.app.aplicacion.puertos.output.EstudiantePersistenciaPuerto;
 import com.servicios.estudiantes.app.dominio.modelo.Estudiante;
+import com.servicios.estudiantes.app.infraestructura.adaptadores.output.persistencia.mapper.EstudiantePersistenciaMapper;
+import com.servicios.estudiantes.app.infraestructura.adaptadores.output.persistencia.repositorio.EstudianteRepositorio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+@Component
+@RequiredArgsConstructor
+public class EstudiantePersistenciaAdaptador  implements EstudiantePersistenciaPuerto {
 
-public class EstudiantePersistenciaAdaptador  implements EstudiantePersistenciaPuerto
-{
+    private final EstudianteRepositorio estudianteRepositorio;
+    private final EstudiantePersistenciaMapper estudiantePersistenciaMapper;
+
     @Override
     public Estudiante crearEstudiante(Estudiante estudiante) {
-        return null;
+        return estudiantePersistenciaMapper.toEstudiante(
+                estudianteRepositorio.save(estudiantePersistenciaMapper.toEstudianteEntidad(estudiante)));
     }
 
     @Override
     public List<Estudiante> obtenerEstudiantes() {
-        return List.of();
+        return estudiantePersistenciaMapper.toEstudiantes(estudianteRepositorio.findAll());
     }
 
     @Override
     public Optional<Estudiante> obtenerEstudiantePorId(Long id) {
-        return Optional.empty();
+        return estudianteRepositorio.findById(id)
+                .map(estudiantePersistenciaMapper::toEstudiante);
     }
 
     @Override
     public void eliminarEstudiante(Long id) {
-
+        estudianteRepositorio.deleteById(id);
     }
 }
--- estudiantes/src/main/java/com/servicios/estudiantes/app/infraestructura/adaptadores/output/persistencia/repositorio/EstudianteRepositorio.java
+++ estudiantes/src/main/java/com/servicios/estudiantes/app/infraestructura/adaptadores/output/persistencia/repositorio/EstudianteRepositorio.java
@@ -1,8 +1,9 @@
 package com.servicios.estudiantes.app.infraestructura.adaptadores.output.persistencia.repositorio;
 
 import com.servicios.estudiantes.app.dominio.modelo.Estudiante;
+import com.servicios.estudiantes.app.infraestructura.adaptadores.output.persistencia.entidades.EstudianteEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EstudianteRepositorio  extends JpaRepository<Estudiante, Long> {
+public interface EstudianteRepositorio  extends JpaRepository<EstudianteEntidad, Long> {
 
 }
